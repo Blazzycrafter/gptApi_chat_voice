97839b69138a396e3d78ec02bfa810eb7a93f92b
Blazzycrafter <kai.knechtl@gmail.com>
2024-08-09 18:51:19 +0100

    added and tested transcribe function in combination with record

--- main.py
+++ main.py
@@ -13,21 +13,40 @@ def record():
         os.remove("voice.ogg")
     except:
         pass
+    print("recording...", end="")
     audio_data = start_recording(
         ffmpegexe=settings.record_ffmpegexe,
         audiodevice=settings.record_audiodevice,
         silent_seconds_stop=settings.record_silent_seconds_stop,
         silence_threshold=settings.record_silence_threshold
     )
+    print("DONE")
+    print("converting...", end="")
     with open("voice.raw", "wb") as f:
         f.write(audio_data.raw_data)
 
-    subprocess.run(f"{settings.record_ffmpegexe} -f s16le -ar 44100 -ac 2 -i voice.raw -c:a libvorbis voice.ogg")
+    subprocess.run(f"{settings.record_ffmpegexe} -f s16le -ar 44100 -ac 2 -i voice.raw -c:a libvorbis voice.ogg",
+                   stdout=subprocess.DEVNULL)
+    print("DONE")
+    print("cleanup...", end="")
     try:
         os.remove("voice.raw")
     except:
         pass
+    print("DONE")
+
+
+def transcribe():
+    print("transcribing...", end="")
+    audio_file = open("voice.ogg", "rb")
+    transcription = openai_client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+    print("DONE")
+    print(transcription.text)
 
 
 if __name__ == '__main__':
     record()
+    transcribe()
